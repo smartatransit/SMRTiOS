8e9c06845cd2d53afdfa4dbf135627864ea66713
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2020-11-26 22:30:41 -0500

    Add bottomsheet

--- smarta/smarta/ViewController.swift
+++ smarta/smarta/ViewController.swift
@@ -4,16 +4,25 @@
 //
 //  Created by Julian A. Fordyce on 11/26/20.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         view.backgroundColor = .white
+        addBottomSheetView()
     }
 
-
+    func addBottomSheetView() {
+        let bottomSheetVC = BottomSheetViewController()
+        
+        self.addChild(bottomSheetVC)
+        self.view.addSubview(bottomSheetVC.view)
+        bottomSheetVC.didMove(toParent: self)
+        
+        let height = view.frame.height
+        let width  = view.frame.width
+        bottomSheetVC.view.frame = CGRect(x: 0, y: view.frame.maxY, width: width, height: height)
+    }
 }
 
